5ceae9264d236925ba699406416a55b740e2083a
djeemie2000 <peter.jamee@telenet.be>
2015-11-11 12:53:59 +0100

    Use export compiler path. Modify paths.

--- mbed-cmake/arm_toolchain.cmake
+++ mbed-cmake/arm_toolchain.cmake
@@ -8,11 +8,9 @@ INCLUDE(CMakeForceCompiler)
 SET(CMAKE_SYSTEM_NAME Generic)
  
 #-------------------------------------------------------------------------------
-# specify the cross compiler, later on we will set the correct path??
-set(TOOLCHN_PATH /home/pj/ToolChains/gcc-arm-none-eabi-4_9-2015q3/bin)
-
-CMAKE_FORCE_C_COMPILER(${TOOLCHN_PATH}/arm-none-eabi-gcc GNU)
-CMAKE_FORCE_CXX_COMPILER(${TOOLCHN_PATH}/arm-none-eabi-g++ GNU)
+# specify the cross compiler, later on we will set the correct path
+CMAKE_FORCE_C_COMPILER(arm-none-eabi-gcc GNU)
+CMAKE_FORCE_CXX_COMPILER(arm-none-eabi-g++ GNU)
 
 
 #-------------------------------------------------------------------------------
